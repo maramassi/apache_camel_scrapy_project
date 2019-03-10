63f2e97935c13f7488c14a6eef85be3c2ac54d30
Maram Assi <assi.maram@gmail.com>
2019-03-11 01:54:56 +0200

    Update ApacheCamelSpider.py

--- ApacheCamelReports/spiders/ApacheCamelSpider.py
+++ ApacheCamelReports/spiders/ApacheCamelSpider.py
@@ -6,42 +6,57 @@ class ApachecamelspiderSpider(scrapy.Spider):
     name = 'ApacheCamelSpider'
     allowed_domains = ['issues.apache.org']
     start_urls = ['https://issues.apache.org/jira/browse/CAMEL-10597/']
-	#setting the location of the output csv file
+    #setting the location of the output csv file
     
     def parse(self, response):
+	    
         #Extract the labels of the detail section
         details_lables=response.css('ul.property-list.two-cols strong.name::text').extract()
+        #removing the : from the labels
         labels_list=[]        
-        for value in details_lables:
-         new=value.replace(':', '')
-         labels_list.append(new)		 
+        for details in details_lables:
+         newDetail=details.replace(':', '')
+         labels_list.append(newDetail)		 
 		 
         #Extract the values of the details section. TODO
-        details_values=response.css('div.value.type-select, div.shorten span, span.labels, span#components-val.value a, span.shorten span, span > ::text').extract()
+        details_values=response.css('#issuedetails span.value::text').extract()
         valueList=[]
         for value in details_values:
          new=value.replace('\n', '').replace('\r', '').replace(' ','')
          valueList.append(new)
 
-		
         #Extract the Labels of the people section
-        people_labels=response.css('div#peoplemodule.module dt::text').extract()
-        #Extract the values of the people section
-        people_values=response.css('div#peoplemodule.module dd > span::text').extract()
-        #Extract the values of the people section
-        dates_values=response.css('div#datesmodule.module div.mod-content dl.dates').extract() 
+        people_labels=response.css('ul#peopledetails li.people-details dt::text').extract()
+        #removing the : from the labels
+        peopleLabelList=[]
+        for value in people_labels:
+         newPeople=value.replace(':', '')
+         peopleLabelList.append(newPeople)
+	
+	#Extract the values of the people section
+        people_values=response.css('#issue_summary_assignee_davsclaus:nth-child(1)::text').extract() 
+        peopleList=[]
+        for people in people_values:
+         newPeopleVal=people.replace('\n', '').replace('\r', '').replace(' ','')
+         peopleList.append(newPeopleVal)
+        print(peopleList)
+        peoplesDict= dict(zip(peopleLabelList, peopleList))
+        #response.css('div#peoplemodule.module dd > span::text').extract()
+        print(peoplesDict)
+        #dates section
+        dates_labels=response.css('div#datesmodule.module > div.mod-content ul li dl.dates dt::text').extract()
+        dates_values=response.css('dd.date time.livestamp::text').extract()
+        newDateLabelList = []
+        for date in dates_labels:
+         newDateLabel=date.replace(':', '')
+         newDateLabelList.append(newDateLabel)
+         
         #extract comments
         #comments_values=response.css('div#comment-15748543.issue-data-block div.action-body').extract()
-        scraped_info = dict(zip(labels_list, valueList))
+        datesDict= dict(zip(newDateLabelList, dates_values))
+        dicDetailsValue = dict(zip(labels_list, valueList))
+        dicDetailsValue.update(datesDict)
+        dicDetailsValue.update(peoplesDict)
+        scraped_info = dicDetailsValue
+        #scraped_info = dict(zip(labels_list, peoplesDict))
         yield scraped_info
-        #lenList = len(details_lables)
-        #print("the lenght of the array")
-        #print(lenList)
-        #for item in details_lables:
-            #create a dictionary to store the scraped info
-        #    scraped_info = {
-        #     'a': item,
-             #'b': item[1]#: details_row_value[0] #item[0]
-        #    }
-            #yield or give the scraped info to scrapy
-        #    yield scraped_info
