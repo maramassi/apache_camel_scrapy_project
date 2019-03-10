60fbde5df326b20f69f9ff44aba56c237fabf2ea
Maram Assi <assi.maram@gmail.com>
2019-03-10 17:01:06 +0200

    Update ApacheCamelSpider.py

--- ApacheCamelReports/spiders/ApacheCamelSpider.py
+++ ApacheCamelReports/spiders/ApacheCamelSpider.py
@@ -4,8 +4,8 @@ import scrapy
 
 class ApachecamelspiderSpider(scrapy.Spider):
     name = 'ApacheCamelSpider'
-    allowed_domains = ['https://issues.apache.org/jira/browse/CAMEL-10597']
-    start_urls = ['http://https://issues.apache.org/jira/browse/CAMEL-10597/']
+    allowed_domains = ['issues.apache.org']
+    start_urls = ['https://issues.apache.org/jira/browse/CAMEL-10597/']
 
     def parse(self, response):
         pass
