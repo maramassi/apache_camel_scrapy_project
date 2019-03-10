34144c981c67d2547589bf32e305543537f67fd4
Maram Assi <assi.maram@gmail.com>
2019-03-10 18:48:24 +0200

    Update ApacheCamelSpider.py

--- ApacheCamelReports/spiders/ApacheCamelSpider.py
+++ ApacheCamelReports/spiders/ApacheCamelSpider.py
@@ -6,20 +6,21 @@ class ApachecamelspiderSpider(scrapy.Spider):
     name = 'ApacheCamelSpider'
     allowed_domains = ['issues.apache.org']
     start_urls = ['https://issues.apache.org/jira/browse/CAMEL-10597/']
-	#setting the location of the output csv file
+    #setting the location of the output csv file
     
     def parse(self, response):
-	    #Extract data using css selectors
-        title=response.css('.toggle-title::text').extract()
-        row_data=zip(title)
+        #Extract data using css selectors
+        #details_row_data=response.xpath("//*[@id='peopledetails']/li[@class='people-details']/dl/text()").extract() 
+        details_row_data=response.css('ul.property-list.two-cols strong.name::text').extract()
+        
+        #for item in details_row_data:
+        #row_data=zip(details_row_data)
 		
         #Making extracted data row wise
-        for item in row_data:
+        for item in details_row_data:
             #create a dictionary to store the scraped info
             scraped_info = {
-                #key:value
-                'page':response.url,
-                'title' : item[0], #item[0] means product in the list and so on, index tells what value to assign, here for testing we have only one
+                'Person' : item #item[0] means product in the list and so on, index tells what value to assign, here for testing we have only one
             }
 
             #yield or give the scraped info to scrapy
