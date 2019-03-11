05409f11483c50f55dd20b9491b5b23b7b1b9654
Maram Assi <assi.maram@gmail.com>
2019-03-11 02:51:49 +0200

    Update ApacheCamelSpider.py

--- ApacheCamelReports/spiders/ApacheCamelSpider.py
+++ ApacheCamelReports/spiders/ApacheCamelSpider.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import time
+import datetime
 
 class ApachecamelspiderSpider(scrapy.Spider):
     name = 'ApacheCamelSpider'
@@ -18,7 +19,7 @@ class ApachecamelspiderSpider(scrapy.Spider):
          newDetail=details.replace(':', '')
          labels_list.append(newDetail)		 
 		 
-        #Extract the values of the details section. TODO
+	#Extract the values of the details section. TODO
         details_values=response.css('#issuedetails span.value::text').extract()
         valueList=[]
         for value in details_values:
@@ -32,20 +33,27 @@ class ApachecamelspiderSpider(scrapy.Spider):
         for value in people_labels:
          newPeople=value.replace(':', '')
          peopleLabelList.append(newPeople)
-	
-	#Extract the values of the people section
-        people_values=response.css('#issue_summary_assignee_davsclaus:nth-child(1)::text').extract() 
+		#people_labels=response.css('div#peoplemodule.module dt::text').extract()
+        #Extract the values of the people section
+        people_values=response.css('span.view-issue-field span.user-hover::text').extract() 
         peopleList=[]
         for people in people_values:
          newPeopleVal=people.replace('\n', '').replace('\r', '').replace(' ','')
          peopleList.append(newPeopleVal)
         print(peopleList)
         peoplesDict= dict(zip(peopleLabelList, peopleList))
-        #response.css('div#peoplemodule.module dd > span::text').extract()
+	#response.css('div#peoplemodule.module dd > span::text').extract()
         print(peoplesDict)
         #dates section
         dates_labels=response.css('div#datesmodule.module > div.mod-content ul li dl.dates dt::text').extract()
         dates_values=response.css('dd.date time.livestamp::text').extract()
+        
+	#TODO: get the epoch format of the date
+	#formatterDates =[]
+	#for dateValue in dates_values:
+        #date_time_obj = datetime.datetime.strptime(dateValue, '%d/%b/%y %H:%M%')  
+        #formatterDates.append( date_time_obj.time()+ ' ' + dateValue);		   
+        #print(formatterDates)
         newDateLabelList = []
         for date in dates_labels:
          newDateLabel=date.replace(':', '')
