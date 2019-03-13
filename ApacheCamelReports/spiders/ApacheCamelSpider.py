3bf694f45d41e1644cf831bf1bac0e009473815b
Maram Assi <assi.maram@gmail.com>
2019-03-13 02:02:15 +0200

    Add files via upload
    Adjusted People section => Done

--- ApacheCamelReports/spiders/ApacheCamelSpider.py
+++ ApacheCamelReports/spiders/ApacheCamelSpider.py
@@ -45,14 +45,12 @@ class ApachecamelspiderSpider(scrapy.Spider):
         details_values=response.css('#issuedetails span.value > span::text').extract() 
         valueList=clearStringList(details_values)
         
-        #Extract the Labels of the people section
-        people_labels=response.css('ul#peopledetails li.people-details dt::text').extract()
-        people_labels=clearStringList(people_labels)
-        
-		#Extract the values of the people section
-        #people_values=response.xpath("//span[@class='user-hover']/text()").extract() 
-        people_values=response.css('span.view-issue-field span.user-hover::text').extract() 
-        people_values=clearStringList(people_values)
+        #the first span is always an image
+        peopleDictNew = {'Assignee' : str(response.css('span#assignee-val.view-issue-field > span.user-hover::text').extract()[1]).replace('\n', '').strip(),
+                         'Reporter' : str(response.css('span#reporter-val.view-issue-field > span.user-hover::text').extract()[1]).replace('\n', '').strip(),
+                         'Votes'    : response.css('#vote-data::text').extract(),
+                         'Watchers'	: response.css('#watcher-data::text').extract()
+                        }						 
         
         #dates section
         dates_labels=clearStringList(response.css('div#datesmodule.module > div.mod-content ul li dl.dates dt::text').extract())
@@ -61,23 +59,24 @@ class ApachecamelspiderSpider(scrapy.Spider):
         #get the epoch format of the date
         epochDates = epochDate(dates_values)
          
-        #extract comments section
-        comments_values=response.xpath('//*[@id="comment-15748543"]/div[1]/div[2]/p/text()').extract()
-        commentsDict=dict(zip('comments', comments_values))
-
+        #TODO extract comments section         //*[@class="activity-comment"]/div[1]/div[2]/p/text()
+        comments_values=response.css('div#comment-15748543.issue-data-block p::text').extract() 
+        print(comments_values)
+        comments_values=" ".join(comments_values)
+        
 		#extract description section
         descriptionValues=response.css('div.user-content-block p::text').extract()
         descriptionValues=" ".join(descriptionValues)
 
 		#building the dictionnaries
         descrDict={'description' : descriptionValues}
+        commentsDict={'comments' : comments_values}
         datesDict= dict(zip(dates_labels, dates_values))
         datesEpochDict=dict(zip(epochDateLabels(dates_labels),epochDates))
-        peoplesDict= dict(zip(people_labels, people_values))
         dicDetailsValue = dict(zip(details_lables, valueList))
         dicDetailsValue.update(datesDict)
         dicDetailsValue.update(datesEpochDict)
-        dicDetailsValue.update(peoplesDict)
+        dicDetailsValue.update(peopleDictNew)
         dicDetailsValue.update(commentsDict)
         dicDetailsValue.update(descrDict)
         
