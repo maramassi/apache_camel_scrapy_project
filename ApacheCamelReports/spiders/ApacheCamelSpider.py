85c7a9946008ab3b382898b2a846fbf27f5ca5fb
Maram Assi <assi.maram@gmail.com>
2019-03-13 23:04:28 +0200

    Add files via upload

--- ApacheCamelReports/spiders/ApacheCamelSpider.py
+++ ApacheCamelReports/spiders/ApacheCamelSpider.py
@@ -8,15 +8,13 @@ class ApachecamelspiderSpider(scrapy.Spider):
     name = 'ApacheCamelSpider'
     allowed_domains = ['issues.apache.org']
     start_urls = ['https://issues.apache.org/jira/browse/CAMEL-10597/']
-	#setting the location of the output csv file
-    
-	#local functions to be used
-	
+	 
     def parse(self, response):	
 
         #function used to get the epoch date from the datetime
         def epochDate(date_time_list):
            date_epoch_list=[]
+           #loop on the list of dates => convert it to milliseconds format
            for date_time_str in date_time_list:
              date_time_obj = dt.strptime(date_time_str, '%d/%b/%y %H:%M').timestamp()
              date_epoch_list.append(date_time_obj)
@@ -24,19 +22,20 @@ class ApachecamelspiderSpider(scrapy.Spider):
 
         def isoTimeFormat(date_time_list):
            date_iso_list=[]
+           #loop on the list of string dates => convert it from string format => convert it to isoformat
            for date_time_str in date_time_list:
              date_time_obj = dt.strptime(date_time_str, '%d/%b/%y %H:%M')
              date_iso_list.append(date_time_obj.isoformat(timespec='microseconds'))
            return date_iso_list		
 		   
-		#function used to get the epoch date lables    
+		#function used to get the epoch date lables. Derived from the date Labels by adding the suffix _Epoch   
         def epochDateLabels(dateLabelsList):
            newList=[]
            for label in dateLabelsList:
             newList.append(label + "_Epoch")
            return newList
 
-		#function used to get rid of the spaces, carriage returns, etc.. of the strings
+		#function used to get rid of the spaces, carriage returns, etc.. of the list of strings
         def clearStringList(strListToClear):
            newClearedList=[]
            for details in strListToClear:
@@ -45,21 +44,20 @@ class ApachecamelspiderSpider(scrapy.Spider):
            return newClearedList
 		  
 		#Extract the labels of the detail section
-        #details_lables=response.css('ul.property-list.two-cols strong.name::text').extract()
         details_lables=clearStringList(response.css('#issuedetails > li > div > strong::text').extract())
          
-		#TODO: Extract correct the values of the details section. TODO
+		#TODO: Extract correct the values of the details section
         details_values=response.css('#issuedetails span.value > span::text').extract() 
         valueList=clearStringList(details_values)
         
-        #the first span is always an image
+        #parsing the people section fields. The first span is always an image for both the Assignee and the Reporter => Select the second element
         peopleDictNew = {'Assignee' : str(response.css('span#assignee-val.view-issue-field > span.user-hover::text').extract()[1]).replace('\n', '').strip(),
                          'Reporter' : str(response.css('span#reporter-val.view-issue-field > span.user-hover::text').extract()[1]).replace('\n', '').strip(),
                          'Votes'    : response.css('#vote-data::text').extract(),
                          'Watchers'	: response.css('#watcher-data::text').extract()
                         }						 
         
-        #dates section
+        #dates section. Get the list of all the fields in the date section as well as their values
         dates_labels=clearStringList(response.css('div#datesmodule.module > div.mod-content ul li dl.dates dt::text').extract())
         dates_values=response.css('dd.date time.livestamp::text').extract()
         
@@ -69,14 +67,17 @@ class ApachecamelspiderSpider(scrapy.Spider):
         #TODO extract comments section         //*[@class="activity-comment"]/div[1]/div[2]/p/text()
         comments_values=response.css('div#comment-15748543.issue-data-block p::text').extract() 
         print(comments_values)
+		#combine all the parsed comments into a single field
         comments_values=" ".join(comments_values)
         
 		#extract description section
         descriptionValues=response.css('div.user-content-block p::text').extract()
+        #combine all the parsed comments into a single field
         descriptionValues=" ".join(descriptionValues)
 
 		#building the dictionnaries
-        descrDict={'description' : descriptionValues}
+        #combining the dictionnaries into one global one
+        descrDict={'description' : descriptionValues} #key : value
         commentsDict={'comments' : comments_values}
         datesDict= dict(zip(dates_labels, isoTimeFormat(dates_values)))
         datesEpochDict=dict(zip(epochDateLabels(dates_labels),epochDates))
