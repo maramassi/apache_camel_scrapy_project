21305615b2d572b6bdc26856c7e2ad225c9808b8
Maram Assi <assi.maram@gmail.com>
2019-03-16 15:20:34 +0200

    Add files via upload

--- ApacheCamelReports/spiders/ApacheCamelSpider.py
+++ ApacheCamelReports/spiders/ApacheCamelSpider.py
@@ -20,15 +20,15 @@ class ApachecamelspiderSpider(scrapy.Spider):
              date_time_obj = dt.strptime(date_time_str, '%Y-%m-%dT%H:%M:%S%z').timestamp()
              date_epoch_list.append(date_time_obj)
            return date_epoch_list
-
-	#function used to get the epoch date lables. Derived from the date Labels by adding the suffix _Epoch   
+	   
+		#function used to get the epoch date lables. Derived from the date Labels by adding the suffix _Epoch   
         def epochDateLabels(dateLabelsList):
            newList=[]
            for label in dateLabelsList:
             newList.append(label + "_Epoch")
            return newList
 
-	#function used to get rid of the spaces, carriage returns, etc.. of the list of strings
+		#function used to get rid of the spaces, carriage returns, etc.. of the list of strings
         def clearStringList(strListToClear):
            newClearedList=[]
            for details in strListToClear:
@@ -36,16 +36,28 @@ class ApachecamelspiderSpider(scrapy.Spider):
             newClearedList.append(newDetail)
            return newClearedList
 		  
-	#Extract the labels of the detail section
-        details_lables=clearStringList(response.css('#issuedetails > li > div > strong::text, ul.property-list > li > div > strong::text').extract())
-         
-	#TODO: Extract correct the values of the details section
-        details_values=response.css('#issuedetails span.value:not(img)::text, span.value span a::text, span.value span::text, ul.property-list > li > div > div> div > span::text, ul.property-list > li > div > div::text').extract() 
-        print(details_values)
-        valueList=clearStringList(details_values)
-        print(valueList)
+        details_values=[]
+		#details_values=response.xpath('#issuedetails span.value:not(img)::text, span.value span a::text, span.value span::text, ul.property-list > li > div > div> div > span::text, ul.property-list > li > div > div::text').extract() 
+        detailsItems =response.xpath("//ul[@id='issuedetails']/li[starts-with(@class, 'item')]") #extracting all the li-details items
+        details_lables=clearStringList(detailsItems.xpath("//div[@class='wrap']/strong/text()").extract())
+        for label in details_lables:
+           if label == "FixVersion/s":
+             details_lables.remove(label)
         
-        #parsing the people section fields. The first span is always an image for both the Assignee and the Reporter => Select the second element
+		#item in detailsItems:
+        #details_values.append(item.xpath('/div[@class="wrap"]/span[@class="value"]/text()[not(normalize-space(.)="")]').extract_first());		
+        #details_values=clearStringList(detailsItems.xpath('//div[@class="wrap"]/span/text()[not(normalize-space(.)="")] | //div[@class="wrap"]/span[@class="value"]/span/text()[not(normalize-space(.)="")]').extract())# | //div[@class="wrap"]/span[@id="resolution-val"]/text() | //div[@class="wrap"]/span[@class="value"]/span/a/text() | //div[@class="wrap"]/span[@class="value"]/span/span/text() | //div[@class="wrap"]/span[@class="value"]/span/text()').extract())
+        details_values=clearStringList(response.css('#issuedetails span.value:not(img)::text, span.value span#components-field a::text, span.value span::text, ul.property-list > li > div > div> div > span::text, ul.property-list > li > div > div::text').extract())# , ul.property-list > li > div > div >span ::text').extract())
+        
+		while '' in details_values:
+           details_values.remove('')
+        print(details_values)     
+
+        while ',' in details_values:
+           details_values.remove(',')
+        print(details_values)     
+
+		#parsing the people section fields. The first span is always an image for both the Assignee and the Reporter => Select the second element
         peopleDictNew = {'Assignee' : str(response.css('span#assignee-val.view-issue-field > span.user-hover::text').extract()[1]).replace('\n', '').strip(),
                          'Reporter' : str(response.css('span#reporter-val.view-issue-field > span.user-hover::text').extract()[1]).replace('\n', '').strip(),
                          'Votes'    : response.css('#vote-data::text').extract(),
@@ -54,28 +66,30 @@ class ApachecamelspiderSpider(scrapy.Spider):
         
         #dates section. Get the list of all the fields in the date section as well as their values
         dates_labels=clearStringList(response.css('div#datesmodule.module > div.mod-content ul li dl.dates dt::text').extract())
-        dates_values=response.css('dd.date time.livestamp::text').extract()
-        #get the epoch format of the date
+        dates_values=response.css('dd.date time.livestamp::attr(datetime)').extract()
+		#get the epoch format of the date
         epochDates = epochDate(dates_values)
          
-        #TODO extract comments section         //*[@class="activity-comment"]/div[1]/div[2]/p/text()
-        comments_values=response.css('div#comment-15748543.issue-data-block p::text').extract() 
-        print(comments_values)
-	#combine all the parsed comments into a single field
+        #TODO extract comments section //*[@class="activity-comment"]/div[1]/div[2]/p/text()
+        comments_values=response.css('div#comment-15748543.issue-data-block p::text').extract()
+        cmt_val= response.css('div.activity-comment div.action-details ::text').extract()		
+        print('***********CMT***************')
+        print(cmt_val)
+        #combine all the parsed comments into a single field
         comments_values=" ".join(comments_values)
         
-	#extract description section
+        #extract description section
         descriptionValues=response.css('div.user-content-block p::text').extract()
         #combine all the parsed comments into a single field
         descriptionValues=" ".join(descriptionValues)
 
-	#building the dictionnaries
-        #combining the dictionnaries into one global one
+		#building the dictionnaries
+        #combining the dictionnaries into a global one
         descrDict={'description' : descriptionValues} #key : value
         commentsDict={'comments' : comments_values}
         datesDict= dict(zip(dates_labels, dates_values))
         datesEpochDict=dict(zip(epochDateLabels(dates_labels),epochDates))
-        dicDetailsValue = dict(zip(details_lables, valueList))
+        dicDetailsValue = dict(zip(details_lables, details_values))
         dicDetailsValue.update(datesDict)
         dicDetailsValue.update(datesEpochDict)
         dicDetailsValue.update(peopleDictNew)
